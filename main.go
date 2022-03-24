cbc9cfda15e42462238dc162d7bef0d2f3cab738
Alexander Böhm <3983539+mheob@users.noreply.github.com>
2022-03-24 18:44:07 +0100

    chore: after lection 1

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"myapp/doctor"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	sayHelloWorld("Hello, sayHelloWorld!")
-}
+	reader := bufio.NewReader(os.Stdin)
 
-func sayHelloWorld(whatToSay string) {
+	whatToSay := doctor.Intro()
+
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print(">> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		}
+
+		fmt.Println(doctor.Response(userInput))
+	}
 }
